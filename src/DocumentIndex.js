c57d74c692caf4bf398627d1163e97851ebedc6f
shamb0t <shams.methnani@gmail.com>
2016-09-27 00:20:06 +0200

    Title substring matching

--- src/DocumentIndex.js
+++ src/DocumentIndex.js
@@ -1,8 +1,27 @@
 'use strict';
 
-const KeyValueIndex = require('orbit-db-kvstore/src/KeyValueIndex');
+class DocumentIndex {
+  constructor() {
+    this._index = {};
+  }
 
-class DocumentIndex extends KeyValueIndex {
+  get(key) {
+    return this._index[key];
+  }
+
+  updateIndex(oplog, added) {
+    added.reverse().reduce((handled, item) => {
+      if (handled.indexOf(item.payload.key) === -1) {
+        handled.push(item.payload.key);
+        if(item.payload.op === 'PUT') {
+          this._index[item.payload.key] = item.payload.value;
+        } else if (item.payload.op === 'DEL') {
+          delete this._index[item.payload.key];
+        }
+      }
+      return handled;
+    }, []);
+  }
 }
 
 module.exports = DocumentIndex;
--- src/DocumentStore.js
+++ src/DocumentStore.js
@@ -1,13 +1,19 @@
 'use strict';
 
-const KeyValueStore = require('orbit-db-kvstore');
+const Store = require('orbit-db-store');
 const DocumentIndex = require('./DocumentIndex');
 
-class DocumentStore extends KeyValueStore {
+class DocumentStore extends Store {
   constructor(ipfs, id, dbname, options) {
     if (!options) options = {};
     if (!options.Index) Object.assign(options, { Index: DocumentIndex });
     super(ipfs, id, dbname, options);
+  }
+
+  get(title) {
+    return Object.keys(this._index._index)
+    .filter((e) => e.indexOf(title) !== -1)
+    .map((e) => this._index.get(e));
   }
 
   put(doc) {
@@ -18,7 +24,18 @@ class DocumentStore extends KeyValueStore {
       meta: {
         ts: new Date().getTime()
       }
-    })
+    });
+  }
+
+  del(key) {
+    return this._addOperation({
+      op: 'DEL',
+      key: key,
+      value: null,
+      meta: {
+        ts: new Date().getTime()
+      }
+    });
   }
 }
 
