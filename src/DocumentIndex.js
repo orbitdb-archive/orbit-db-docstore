3c0590230a956547daa2f1c6e65a0855d7909806
Keisuke Kanao <keisuke.kanao.154@gmail.com>
2018-07-05 11:12:31 +0900

    Fixed Performance issue of docstore #13: https://github.com/orbitdb/orbit-db-docstore/issues/13

--- src/DocumentIndex.js
+++ src/DocumentIndex.js
@@ -11,7 +11,17 @@ class DocumentIndex {
       : this._index[key].payload.value
   }
 
+  _updateIndex (oplog, entry) {
+    if (this._index[entry.payload.key] === undefined || this._index[entry.payload.key].clock.time < entry.clock.time) {
+      if (entry.payload.op === 'PUT')
+        this._index[entry.payload.key] = entry
+      else if (entry.payload.op === 'DEL' && this._index[entry.payload.key] !== undefined)
+        delete this._index[entry.payload.key]
+    }
+
   updateIndex (oplog, onProgressCallback) {
+    if (typeof onProgressCallback == 'object')
+      return this._updateIndex(oplog, onProgressCallback)
     const reducer = (handled, item, idx) => {
       if (handled[item.payload.key] !== true) {
         handled[item.payload.key] = true
