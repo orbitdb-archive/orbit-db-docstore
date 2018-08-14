ba48672de5261753e53d6fdb5e4ae9cb04808b9b
shamb0t <shams.methnani@gmail.com>
2018-08-14 16:48:51 +0200

    add to lunr
    refactor
    
    add events
    
    add loading

--- src/DocumentIndex.js
+++ src/DocumentIndex.js
@@ -1,14 +1,22 @@
 'use strict'
+const elasticlunr = require('elasticlunr')
 
 class DocumentIndex {
   constructor () {
     this._index = {}
+    this._lunr = elasticlunr()
   }
+
+  get (key, fullOp = false) {
+     return fullOp
+       ? this._index[key]
+       : this._index[key] ? this._index[key].payload.value : null
+   }
 
   updateIndex (oplog, onProgressCallback) {
     const reducer = (handled, item, idx) => {
-      if (handled[item.payload.key] !== true) {
-        handled[item.payload.key] = true
+      if (handled[item.hash] !== true) {
+        handled[item.hash] = true
         if(item.payload.op === 'PUT') {
           this._index[item.hash] = item
         } else if (item.payload.op === 'DEL') {
--- src/DocumentStore.js
+++ src/DocumentStore.js
@@ -3,10 +3,7 @@
 const Store = require('orbit-db-store')
 const DocumentIndex = require('./DocumentIndex')
 const pMap = require('p-map')
-const Readable = require('readable-stream')
 const elasticlunr = require('elasticlunr')
-
-const replaceAll = (str, search, replacement) => str.toString().split(search).join(replacement)
 
 class DocumentStore extends Store {
   constructor (ipfs, id, dbname, options) {
@@ -14,52 +11,45 @@ class DocumentStore extends Store {
     if (!options.indexBy) Object.assign(options, { indexBy: ['hash'] })
     if (!options.Index) Object.assign(options, { Index: DocumentIndex })
     super(ipfs, id, dbname, options)
-    this._createSearchIndex(options.indexBy)
+    this._addToLunr(options.indexBy)
     this._type = 'docstore'
   }
 
-  _createSearchIndex(indexKeys = ['_id']) {
-    this.searchIndex = elasticlunr()
+  _addToLunr(indexKeys) {
     indexKeys.forEach((indexBy) => {
-      this.searchIndex.addField(indexBy)
+      this._index._lunr.addField(indexBy)
     })
-    this.searchIndex.setRef('hash')
+    this._index._lunr.setRef('hash')
+    this.events.addListener('replicate', (address, entry) => {
+      const idxDoc = Object.assign({}, entry.payload.value, { hash: entry.hash })
+      this._index._lunr.addDoc(idxDoc)
+    })
+    this.events.addListener('load.progress', (addr, hash, entry, prog, max) => {
+      const idxDoc = Object.assign({}, entry.payload.value, { hash: entry.hash })
+      this._index._lunr.addDoc(idxDoc)
+    })
   }
 
-  get (key, caseSensitive = false) {
+  get (key, options = { fullOp : false }) {
     key = key.toString()
-    // const terms = key.split(' ')
-    // key = terms.length > 1 ? replaceAll(key, '.', ' ').toLowerCase() : key.toLowerCase()
-    //
-    // const search = (e) => {
-    //   if (terms.length > 1) {
-    //     return replaceAll(e, '.', ' ').toLowerCase().indexOf(key) !== -1
-    //   }
-    //   return e.toLowerCase().indexOf(key) !== -1
-    // }
-    const docs = this.searchIndex.search(key).map(e => e.ref)
-    const mapper = e => this._index.get(e)
-    const filter = e => docs.includes(e.hash)
-
-    return docs.map(e => this._index._index[e])
-    // return Object.keys(this._index._index)
-    //   .filter(filter)
-    //   .map(mapper)
+    return this._index._lunr.search(key, options)
+      .map(e => e.ref)
+      .map(e => this._index._index[e])
+      .filter(e => e !== undefined && e !== null)
+      .map(e => options.fullOp ? e : e.payload.value)
   }
 
   query (mapper, options = {}) {
     // Whether we return the full operation data or just the db value
     const fullOp = options ? options.fullOp : false
-
     return Object.keys(this._index._index)
-      .map((e) => this._index.get(e, fullOp))
+      .map((e) => this.get(e, { fullOp }))
       .filter(mapper)
   }
 
   batchPut (docs, onProgressCallback) {
-    const mapper = (doc, idx) => {
-      return this._addOperationBatch(
-        {
+    const mapper = async (doc, idx) => {
+      const hash = await this._addOperationBatch({
           op: 'PUT',
           value: doc
         },
@@ -67,6 +57,9 @@ class DocumentStore extends Store {
         idx === docs.length - 1,
         onProgressCallback
       )
+      const idxDoc = Object.assign({}, doc, { hash: hash })
+      this._index._lunr.addDoc(idxDoc)
+      return hash
     }
 
     return pMap(docs, mapper, { concurrency: 1 })
@@ -78,14 +71,14 @@ class DocumentStore extends Store {
     if (indexCount < 1)
       throw new Error(`The provided document doesn't contain any field from'${this.options.indexBy}'`)
 
-    doc.hash = await this._addOperation({
+    const hash = await this._addOperation({
       op: 'PUT',
-      key: null,
       value: doc
     })
 
-    this.searchIndex.addDoc(doc)
-    return doc.hash
+    const idxDoc = Object.assign({}, doc, { hash: hash })
+    this._index._lunr.addDoc(idxDoc)
+    return hash
   }
 
   del (key) {
