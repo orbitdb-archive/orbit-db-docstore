b3f65f58a2432d2ac1362bca1d0063659e16e4c9
Huy Hoang <huy.hoang711@gmail.com>
2018-08-31 13:34:14 -0500

    tested and debugged

--- src/DocumentStore.js
+++ src/DocumentStore.js
@@ -4,6 +4,7 @@ const Store = require('orbit-db-store')
 const DocumentIndex = require('./DocumentIndex')
 const pMap = require('p-map')
 const Readable = require('readable-stream')
+const _ = require('underscore')
 
 const replaceAll = (str, search, replacement) => str.toString().split(search).join(replacement)
 
@@ -31,27 +32,21 @@ class DocumentStore extends Store {
 
     // Expected input for indexBy is {indexBy: {field1: type1, field2: type2}}
     this._indexedFields = {}
+    this._indexes = {}
+    var self = this
     Object.keys(indexBy).forEach(function(index) {
-        this._indexedFields[index].type = options.indexBy[index]
-      })
+      self._indexes[index] = {}
+      self._indexedFields[index] = {}
+      self._indexedFields[index]['type'] = options.indexBy[index]
+    })
     this._type = 'docstore'
 
   }
 
-  get (key, caseSensitive = false) {
-    key = key.toString()
-    const terms = key.split(' ')
-    key = terms.length > 1 ? replaceAll(key, '.', ' ').toLowerCase() : key.toLowerCase()
-
-    const search = (e) => {
-      if (terms.length > 1) {
-        return replaceAll(e, '.', ' ').toLowerCase().indexOf(key) !== -1
-      }
-      return e.toLowerCase().indexOf(key) !== -1
-    }
+  get (_id, caseSensitive = false) {    
     const mapper = e => this._index.get(e)
     const filter = e => caseSensitive
-      ? e.indexOf(key) !== -1 
+      ? e.indexOf(_id) !== -1 
       : search(e)
 
     return Object.keys(this._index._index)
@@ -60,49 +55,56 @@ class DocumentStore extends Store {
   }
 
   query (queryObj, options = {}) {
+    var self = this
     // Whether we return the full operation data or just the db value
     // const fullOp = options ? options.fullOp : false
     var shortlisted_result = {}
     var unindexed_fields = []
+    var reduced_queryObj = {}
+
     Object.keys(queryObj).forEach(function(field) {
-      if (this._indexedFields[field]) {
+      if (self._indexedFields[field]) {
         var value_to_retrieve = queryObj[field]
-        if (!this._indexes[field][value_to_retrieve]){
+        if (!self._indexes[field][value_to_retrieve]){
           // if there's no matching value, return right away
           return []
         } else {
-          shortlisted_result[field] = this._indexes[field][value_to_retrieve]
+          shortlisted_result[field] = self._indexes[field][value_to_retrieve]
         }
         
       } else {
-        unindexed_fields.push(field)
+        reduced_queryObj[field] = queryObj[field]
       }
     });
-
-    var reduced_queryObj = {}
-    unindexed_fields.forEach(function(unindexed_field) {
-      reduced_queryObj[unindexed_field] = queryObj[unindexed_field]
-    })
-
-    // Consolidate shortlisted results to one final array
+    console.log(reduced_queryObj)
+    
+    
     var id_array_to_search = []
-    Object.keys(shortlisted_result).forEach(function(field) {
-      var current_field_array = shortlisted_result[field]
-      current_field_array.forEach(function(hash_value) {
-        if (!array_to_search.includes(hash_value)) {
-          array_to_search.push(hash_value)
+    if (JSON.stringify(queryObj) === JSON.stringify(reduced_queryObj)) {
+      // If the query contains no indexed field, scan the whole store
+      console.log("No indexed fields found in the query object, gotta scan the whole store")
+      id_array_to_search = Object.keys(self._index._index)
+    } else {
+      // If the query contains at least one indexed field,
+      // consolidate shortlisted results to one final array 
+      Object.keys(shortlisted_result).forEach(function(field) {
+        var current_field_array = shortlisted_result[field]
+        if (id_array_to_search.length == 0) {
+          id_array_to_search = current_field_array
+        } else {
+          id_array_to_search = _.intersection(id_array_to_search, current_field_array)
         }
+        
       })
-    })
+    }    
 
-    var array_to_search = []
-    id_array_to_search.map(e => array_to_search.push(this._index.get(e)))
-    console.log(array_to_search)
+    var record_array_to_search = []
+    id_array_to_search.map(e => record_array_to_search.push(this._index.get(e)))
 
     var value_to_search = []
     var results = []
     // Find matching records in shortlisted_results
-    array_to_search.forEach(function(record) {
+    record_array_to_search.forEach(function(record) {
       var matched = true
       for (var field in reduced_queryObj) {
         if (record[field] != reduced_queryObj[field]) {
@@ -111,12 +113,11 @@ class DocumentStore extends Store {
         }
       }
       if (matched == false)
-        continue
+        return
       else
         results.push(record)
       matched = true
     })
-
     return results
   }
 
@@ -139,55 +140,60 @@ class DocumentStore extends Store {
   }
 
   put (doc) {
+    var self = this
     // If there is no _id in the object, create one using the current timestamp
     // precise to the mili-secondth to make sure no two _id's are the same
     if (!doc['_id'])
       doc['_id'] = Date.getTime().toString()
 
     // Check if all indexed fields exists in the input object except for _id
-    Object.keys(this._indexedFields).forEach(function(index) {
+    Object.keys(self._indexedFields).forEach(function(index) {
       if (index != '_id' && !doc[index])
         throw new Error(`Field '${index}' doesn't exist in the object.`)
     });
     
     // Modifying the indexes with new document
-    Object.keys(this._indexedFields).forEach(function(index) {      
-      doc_value_to_index = doc[index]
-      if (!this._indexes[index][doc_value_to_index])
-        this._indexes[index][doc_value_to_index] = []
-      this._indexes[index][doc_value_to_index].push(doc._id)
+    Object.keys(self._indexedFields).forEach(function(index) {      
+      var doc_value_to_index = doc[index]
+      if (!self._indexes[index][doc_value_to_index])
+        self._indexes[index][doc_value_to_index] = []
+      self._indexes[index][doc_value_to_index].push(doc._id)
       
       // If the index type is numeric, update the max and min value
       // FUTURE DEV: index float values in ranges
-      if (this._indexedFields[index][type] == 'numeric') {
-        if (!this._indexedFields[index].min || !this._indexedFields[index].max) {
-          this._indexedFields[index].min = doc_value_to_index
-          this._indexedFields[index].max = doc_value_to_index
+      if (self._indexedFields[index].type == 'numeric') {
+        if (!self._indexedFields[index].min || !self._indexedFields[index].max) {
+          self._indexedFields[index].min = doc_value_to_index
+          self._indexedFields[index].max = doc_value_to_index
         } else {
-          if (doc_value_to_index < this._indexedFields[index].min)
-            this._indexedFields[index].min = doc_value_to_index
-          else if (doc_value_to_index > this._indexedFields[index].max)
-            this._indexedFields[index].max = doc_value_to_index
+          if (doc_value_to_index < self._indexedFields[index].min)
+            self._indexedFields[index].min = doc_value_to_index
+          else if (doc_value_to_index > self._indexedFields[index].max)
+            self._indexedFields[index].max = doc_value_to_index
         }        
       }
     })
 
-    return this._addOperation({
+    return self._addOperation({
       op: 'PUT',
       key: doc._id,
       value: doc
     })
   }
 
-  del (key) {
-    if (!this._index.get(key))
-      throw new Error(`No entry with key '${key}' in the database`)
+  del (_id) {
+    if (!this._index.get(_id))
+      throw new Error(`No entry with _id '${_id}' in the database`)
 
     return this._addOperation({
       op: 'DEL',
-      key: key,
+      key: _id,
       value: null
     })
+  }
+
+  getIndex() {
+    return this._indexedFields
   }
 }
 
